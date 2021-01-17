b461e91b99dc8e2d9b4e5597fc3f5723c19fd71a
Boji Hao <boji.hao@greencap.com.au>
2021-01-17 22:50:46 +1100

    Disable buttons based on game status

--- src/features/bulldozerGame/ControlContainer.spec.tsx
+++ src/features/bulldozerGame/ControlContainer.spec.tsx
@@ -3,9 +3,70 @@ import React from "react";
 import { Provider } from "react-redux";
 import configureMockStore from "redux-mock-store";
 import ControlContainer from "./ControlContainer";
+import { GameStatus } from "./state/constant";
+import { initialGameState } from "./state/gameReducer";
 
-test("game control ui", () => {
-  const s = configureMockStore()();
+test("game button status before bulldozer on site", () => {
+  const s = configureMockStore()({
+    game: {
+      ...initialGameState,
+      status: GameStatus.Starting,
+    },
+  });
+  const c = render(
+    <Provider store={s}>
+      <ControlContainer />
+    </Provider>
+  );
+  expect(c.getByRole("button", { name: /advance/i })).not.toBeDisabled();
+  expect(c.getByRole("button", { name: /left/i })).toBeDisabled();
+  expect(c.getByRole("button", { name: /right/i })).toBeDisabled();
+  expect(c.getByRole("button", { name: /quit/i })).toBeDisabled();
+});
+
+test("game button status after bulldozer on site", () => {
+  const s = configureMockStore()({
+    game: {
+      ...initialGameState,
+      status: GameStatus.Started,
+    },
+  });
+  const c = render(
+    <Provider store={s}>
+      <ControlContainer />
+    </Provider>
+  );
+  expect(c.getByRole("button", { name: /advance/i })).not.toBeDisabled();
+  expect(c.getByRole("button", { name: /left/i })).not.toBeDisabled();
+  expect(c.getByRole("button", { name: /right/i })).not.toBeDisabled();
+  expect(c.getByRole("button", { name: /quit/i })).not.toBeDisabled();
+});
+
+test("button status upon ending game", () => {
+  const s = configureMockStore()({
+    game: {
+      ...initialGameState,
+      status: GameStatus.Ended,
+    },
+  });
+  const c = render(
+    <Provider store={s}>
+      <ControlContainer />
+    </Provider>
+  );
+  expect(c.getByRole("button", { name: /advance/i })).toBeDisabled();
+  expect(c.getByRole("button", { name: /left/i })).toBeDisabled();
+  expect(c.getByRole("button", { name: /right/i })).toBeDisabled();
+  expect(c.getByRole("button", { name: /quit/i })).toBeDisabled();
+});
+
+test("game control action", () => {
+  const s = configureMockStore()({
+    game: {
+      ...initialGameState,
+      status: GameStatus.Started,
+    },
+  });
   const c = render(
     <Provider store={s}>
       <ControlContainer />
--- src/features/bulldozerGame/ControlContainer.tsx
+++ src/features/bulldozerGame/ControlContainer.tsx
@@ -1,15 +1,22 @@
 import { makeStyles, ButtonProps, Button, Paper } from "@material-ui/core";
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import styles from "./ControlContainer.styles";
 import { advanceBulldozer, rotateBulldozer } from "./state/bulldozerActions";
-import { Command, ROTATION_LEFT, ROTATION_RIGHT } from "./state/constant";
+import {
+  Command,
+  GameStatus,
+  ROTATION_LEFT,
+  ROTATION_RIGHT,
+} from "./state/constant";
 import { endSimuation } from "./state/siteActions";
 import config from "../../app/config";
+import { AppState } from "../../app/rootReducer";
 
 const GameControl = (): JSX.Element => {
   const classes = makeStyles(styles)();
   const dispatch = useDispatch();
+  const status = useSelector((s: AppState) => s.game.status);
   const handleAdvance = () => dispatch(advanceBulldozer());
   const handleLeft = () => dispatch(rotateBulldozer(ROTATION_LEFT));
   const handleRight = () => dispatch(rotateBulldozer(ROTATION_RIGHT));
@@ -18,10 +25,15 @@ const GameControl = (): JSX.Element => {
     variant: "outlined",
     color: "primary",
     className: classes.button,
+    disabled: status !== GameStatus.Started,
   };
   return (
     <Paper className={classes.root} variant="outlined">
-      <Button {...buttonProps} onClick={handleAdvance}>
+      <Button
+        {...buttonProps}
+        disabled={status === GameStatus.Ended || status === GameStatus.Error}
+        onClick={handleAdvance}
+      >
         {config.commandName[Command.Advance]}
       </Button>
       <Button {...buttonProps} onClick={handleLeft}>
